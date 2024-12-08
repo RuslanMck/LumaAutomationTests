d43bde49e1d4efda161b434d27534b60e2524e4e
RuslanMarchuk <rusmar@qamadness.com>
2024-12-08 23:20:27 +0100

    deleting not needed logs

--- src/test/java/ui/TestConfig.java
+++ src/test/java/ui/TestConfig.java
@@ -4,16 +4,13 @@ import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.WebDriverRunner;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
-import org.testng.Assert;
 
 import java.net.URL;
 import java.util.Map;
 
 import java.net.MalformedURLException;
-import java.net.URI;
 import java.util.HashMap;
 
-import static com.fasterxml.jackson.databind.type.LogicalType.Map;
 
 public abstract class TestConfig {
 
@@ -30,24 +27,12 @@ public abstract class TestConfig {
             selenoidOptions.put("enableVideo", true);
             capabilities.setCapability("selenoid:options", selenoidOptions);
 
-            // Log the capabilities being used
-            System.out.println("Capabilities: " + capabilities);
-
             RemoteWebDriver driver = new RemoteWebDriver(new URL("http://172.18.0.3:4444/wd/hub"), capabilities);
 
-
-            // Log successful WebDriver initialization
+            // Log to check if the WebDriver has successful initialization
             System.out.println("WebDriver initialized successfully.");
 
             WebDriverRunner.setWebDriver(driver);
-
-            // Open the URL
-//            String testUrl = "https://www.marktplaats.nl/cp/537/witgoed-en-apparatuur/";
-//            driver.get(testUrl);
-//            Assert.assertEquals(driver.getCurrentUrl(), "https://www.marktplaats.nl/cp/537/witgoed-en-apparatuur/", "URL did not match!");
-//
-//            // Log the URL being opened
-//            System.out.println("Opened URL: " + testUrl);
 
         } catch (MalformedURLException e) {
             e.printStackTrace();
@@ -56,7 +41,5 @@ public abstract class TestConfig {
             e.printStackTrace();
             System.err.println("An error occurred during WebDriver setup: " + e.getMessage());
         }
-
-
     }
 }
